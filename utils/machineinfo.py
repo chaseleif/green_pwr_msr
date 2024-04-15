7858f2577745293cfc1903e01535a0763a8add20
chaseleif <chaseleif@icloud.com>
2024-04-14 21:41:08 -0500

    Incorporate upstream changes - new pickle/csv files and slightly different machine info

--- utils/machineinfo.py
+++ utils/machineinfo.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import cpuinfo, os, psutil
+import cpuinfo, os, psutil, threading
 
 def getmachinespec(csvname='cpuinfo', new=False):
   if not new and os.path.isfile(csvname):
@@ -11,18 +11,14 @@ def getmachinespec(csvname='cpuinfo', new=False):
         parts = line.rstrip().split()
         machine[parts[0]] = cast(parts[0], parts[1])
   else:
-    # The multiplier, 2, for number_of_threads is copied from the original
-    # os.cpu_count() returns logical cores already, unsure of the intent
-    # The original misclassified my Intel(R) as other, since it != 'Intel'
-    # processor was unconditionally set to -1 in the original
     machine = cpuinfo.get_cpu_info()['brand_raw']
     machine = { 'number_of_cores':os.cpu_count(),
-                'number_of_threads':os.cpu_count()*2,
+                'number_of_threads':os.cpu_count()*threading.active_count(),
                 'frequency':psutil.cpu_freq()[2]/1000,
                 'processor_manufacturer':0 if 'AMD' in machine \
                                     else 1 if 'Intel' in machine \
                                     else 2,
-                'processor':-1 }
+                'processor':18 }
     with open(csvname,'w') as outfile:
       for key, val in machine.items():
         outfile.write(f'{key} {val}\n')
